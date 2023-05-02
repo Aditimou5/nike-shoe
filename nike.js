323931ff719990c41f9feef934138842549fff29
aditi <sarkaraditi106@gmail.com>
2023-05-02 22:09:40 +0530

    second commit

---
+++ nike.js
@@ -0,0 +1,6 @@
+let cont=document.querySelector('.h2')
+//console.log(cont)
+//let node=cont.children[5].children
+console.log(cont.firstChild)
+console.log(cont.firstElementChild)
+console.log(cont.childElementCount)
